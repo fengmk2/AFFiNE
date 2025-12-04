6cd019a00ec2ba746be4f99cd927e63bbe7c44dc
MK <fengmk2@gmail.com>
2025-12-04 09:54:06 +0800

    chore: vite+ migration

--- packages/frontend/apps/electron/vitest.config.ts
+++ packages/frontend/apps/electron/vitest.config.ts
@@ -3,7 +3,7 @@ import { resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from '@voidzero-dev/vite-plus';
 
 const rootDir = fileURLToPath(new URL('../../../..', import.meta.url));
 
